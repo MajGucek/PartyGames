181242d04d5f3a431648235f1f4cf6a446143601
MajGucek <113456426+MajGucek@users.noreply.github.com>
2025-12-09 20:17:19 +0100

    Implemented loading from file, have to Implement converting Sprite field to 3D list

--- Client/src/main/java/org/PartyGames/Client/Games/NullGame/NullGame.java
+++ Client/src/main/java/org/PartyGames/Client/Games/NullGame/NullGame.java
@@ -1,19 +1,28 @@
 package org.PartyGames.Client.Games.NullGame;
 
 import org.PartyGames.Client.Games.GameClientController;
+import org.PartyGames.Client.Sprites.Sprite;
 import org.PartyGames.Common.Networking.NetworkMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class NullGame extends GameClientController {
     private static final Logger logger = LoggerFactory.getLogger(NullGame.class);
+    private Sprite test;
 
     public NullGame() {
         super();
         logger.warn("You've instantiated a NullGame Object, Beware!");
+        this.test = new Sprite("penguin");
+        try {
+            this.test.loadSprite();
+        } catch (FileNotFoundException e) {
+            logger.error("Couldn't load file: {}", String.valueOf(e));
+        }
     }
 
     @Override
@@ -28,6 +37,9 @@ public class NullGame extends GameClientController {
 
     @Override
     public void handleGame(List<NetworkMessage> messages, int tick) {
+        if (test.isLoaded()) {
+            logger.info("Width: {}", test.sprite.width);
+        }
         io_controller.clearScreen();
         io_controller.drawText(2, 5, "Waiting for a Connection!", "rgb(255, 255, 255)");
         io_controller.render();
--- Client/src/main/java/org/PartyGames/Client/Sprites/Sprite.java
+++ Client/src/main/java/org/PartyGames/Client/Sprites/Sprite.java
@@ -1,5 +1,88 @@
 package org.PartyGames.Client.Sprites;
 
-public class Sprite {
+import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+
+public class Sprite {
+    protected String file_name;
+    protected boolean is_loaded;
+    public RawSprite sprite;
+
+    public Sprite(String file_name) {
+        this.file_name = file_name;
+        this.is_loaded = false;
+    }
+
+    private static String getFullFileName(String file_name) {
+        return System.getProperty("user.dir") + "/assets/" + file_name + ".guspr.json";
+    }
+
+    @SuppressWarnings("unused")
+    public final static class RawSprite {
+        public int frame_count;
+        public int width;
+        public int height;
+        public List<List<List<Character>>> frames;
+    }
+
+
+    public boolean isLoaded() { return this.is_loaded; }
+
+    public void loadSprite() throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(getFullFileName(this.file_name)));
+        Gson gson = new Gson();
+        this.sprite = gson.fromJson(reader, RawSprite.class);
+        this.is_loaded = true;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
